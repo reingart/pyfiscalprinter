b55d35a7ce1fca0d9ea9e92be70d8bcf3ba83b36
Mariano Reingart <reingart@gmail.com>
2018-06-03 14:12:48 -0300

    Controlador: soportar descuentos/recargo (por item y adicionales)

--- controlador.py
+++ controlador.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart <reingart@gmail.com>"
 __copyright__ = "Copyright (C) 2014 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.06c"
+__version__ = "1.06d"
 
 CONFIG_FILE = "fiscal.ini"
 DEBUG = True
